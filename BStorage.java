0c03b74c440118529ee20ddfe2ff0697b061ab68
mwiesman <mfwiesman@gmail.com>
2015-09-12 16:07:21 -0400

    Create BStorage.java

---
+++ BStorage.java
@@ -0,0 +1,34 @@
+package com.mfwiesman.beeroclock;
+
+import java.util.Map;
+
+public class BStorage {
+    private static Map<String, Long> beers = null;
+
+    private static long currentTime = 0;
+    private static String currentBeer = null;
+
+    public static Map<String, Long> getBeers() {
+        return beers;
+    }
+
+    public static long getCurrentTime() {
+        return currentTime;
+    }
+
+    public static String getCurrentBeer() {
+        return currentBeer;
+    }
+
+    public static void setBeers(Map<String, Long> beers) {
+        BStorage.beers = beers;
+    }
+
+    public static void setCurrentTime(long currentTime) {
+        BStorage.currentTime = currentTime;
+    }
+
+    public static void setCurrentBeer(String currentBeer) {
+        BStorage.currentBeer = currentBeer;
+    }
+}
